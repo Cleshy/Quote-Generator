e4045cb0fd8a6e541cfdc4652a98e4d6ae9f8bff
Czipa Szilárd <szilard.czipa@gmail.com>
2023-12-25 20:08:20 +0100

    New Quote & Twitter Button

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 "use strict";
 
+const quoteText = document.querySelector("#quote");
+const authorText = document.querySelector("#author");
+const twitterBtn = document.querySelector("#twitter");
+const newQuoteBtn = document.querySelector("#new-quote");
+
 // Global variable for json response.
 let apiQuotes = [];
+let quote = "";
 
 // Show new Quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author;
+
+  // Check Quote length to determine styling
+  if (quote.text.length > 100) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
@@ -22,6 +37,16 @@ async function getQuotes() {
     console.error(error);
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 getQuotes();
