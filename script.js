b2af04e9ab1872acb2e0eb4a81e10235cecd4a1f
Czipa Szilárd <szilard.czipa@gmail.com>
2023-12-25 21:06:34 +0100

    Loading animation & Own Styles

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 "use strict";
 
+const quoteContainer = document.querySelector("#quote-container");
 const quoteText = document.querySelector("#quote");
 const authorText = document.querySelector("#author");
 const twitterBtn = document.querySelector("#twitter");
 const newQuoteBtn = document.querySelector("#new-quote");
+const loader = document.querySelector("#loader");
 
 // Global variable for json response.
 let apiQuotes = [];
 let quote = "";
 
+// Show loading
+function quoteLoading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function quoteLoaded() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new Quote
 function newQuote() {
+  quoteLoading();
   // Pick a random quote from apiQuotes array
   quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   authorText.textContent = quote.author;
@@ -21,12 +36,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
-
+  // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  quoteLoaded();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+  quoteLoading();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
